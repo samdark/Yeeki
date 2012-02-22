ae6dba4b221d362aecfc154a35d17f4fd1f5ad0b
Alexander Makarov <sam@rmcreative.ru>
2012-02-22 04:58:03 +0400

    added date and time to version history, added link back to page from version history

--- app/modules/wiki/views/default/history.php
+++ app/modules/wiki/views/default/history.php
@@ -1,12 +1,18 @@
-<h1><?php echo CHtml::encode($page->getWikiUid())?> <?php echo Yii::t('wiki', 'version history')?></h1>
+<h1><?php echo CHtml::link(CHtml::encode($page->getWikiUid()), array('view', 'uid' => $page->getWikiUid()))?> <?php echo Yii::t('wiki', 'version history')?></h1>
 
 <?php echo CHtml::beginForm()?>
 <table>
-	<tr></tr>
+	<tr>
+		<th></th>
+		<th>Revision</th>
+		<th>Created at</th>
+		<th>Comment</th>
+	</tr>
 	<?php foreach($revisions as $revision):?>
 	<tr>
 		<td><?php echo CHtml::checkBox('r'.$revision->id)?></td>
 		<td><?php echo CHtml::link('r'.$revision->id, array('view', 'uid' => $page->page_uid, 'rev' => $revision->id))?></td>
+		<td><?php echo Yii::app()->format->formatDatetime($revision->created_at)?></td>
 		<td><?php echo CHtml::encode($revision->comment)?></td>
 	</tr>
 	<?php endforeach ?>
